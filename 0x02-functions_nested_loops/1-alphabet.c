da16eb0a355246dfb1ea5f538f4c618622288323
S-ergon <sergontoo99@gmail.com>
2022-12-15 10:44:00 -0800

    islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,15 @@
 #include "main.h"
 
-
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *
+ * print_alphabet - prints all alphabet in lowercase
  */
-void print_alphabet(void)
+
+oid print_alphabet(void);
 {
-
-
 	char letter;
 
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
-		_putchar(letter);
-
-
-	_putchar('\n');
-
-}
+	di
+		
 
 
 
