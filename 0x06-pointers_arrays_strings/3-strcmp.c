f1dedfd6506bf68d48f0cac146fdd571074ee186
S-ergon <sergontoo99@gmail.com>
2022-12-22 02:22:36 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,17 +2,18 @@
 
 /**
  * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
+ * @s1: pointer
+ * @s2: pointer
  *
- * Return: s1[i] - s2[i]
+ * Return: j-integer
  */
+int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
 	j = 0;
 
-	for (i = 0; s1[i] != "\0" && s2[i] != "\0"; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
